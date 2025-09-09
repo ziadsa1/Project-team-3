599868bee519f15431fa448f34c1a1bc42c8ec29
ZIAD <ziadsaid525@gmail.com>
2025-09-09 18:45:48 +0300

    first commit

---
+++ project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Login from "./components/LoginPage"
+export default function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/login">Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
---
+++ project/src/components/LoginPage.jsx
@@ -0,0 +1,10 @@
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default LoginPage;
